22fe98fbaf826327a1a3bc7e753485462cd74d4a
Tombstone1922 <114335234+Tombstone1922@users.noreply.github.com>
2026-06-05 22:27:11 +0400

    Import RPD diagnostics styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './analysis.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
